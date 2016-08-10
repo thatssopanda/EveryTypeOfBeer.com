c361e8449432bfe05d7a128043a3816056b354a5
Daniel Williams <thatssopanda@gmail.com>
2016-08-10 14:06:23 -0400

    Uploaded bootstrap theme

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Main extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -21,7 +21,9 @@ class Welcome extends CI_Controller {
 	 * Learning CodeIgniter at //https://www.youtube.com/watch?v=rBdqTHAGHfU
 	 */
 	public function index()
-	{
-		$this->load->view('welcome_message');
+	{		
+		$this->load->view('header');
+		$this->load->view('content');
+		$this->load->view('footer');
 	}
 }
